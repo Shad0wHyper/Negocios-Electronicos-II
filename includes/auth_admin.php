7c4676e67af235c42e4d9bb3acc01a2acdc81652
Shad0wHyper <luisitogarduno.ext@gmail.com>
2026-09-17 09:44:17 -0600

    admin cloud

--- includes/auth_admin.php
+++ includes/auth_admin.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/config.php';
 // Si no hay una sesión de usuario o el rol no es 'admin'
 if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
     // Redirige a la página de login
-    header('Location: login.php');
+    header('Location: ../login.php');
     exit;
 }
 ?>
